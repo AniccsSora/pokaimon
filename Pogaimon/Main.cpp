6d8b14b3035140ef3453a2e032bf2e44b54f644d
aniccasora <aniccasora@gmail.com>
2020-05-26 21:04:55 +0800

    加了一些 util

--- Pogaimon/Main.cpp
+++ Pogaimon/Main.cpp
@@ -31,29 +31,10 @@ int main() {
     // 呼叫 resetColor()，可以 回到 saveDefaultColor() 當時所記錄的顏色。
 	//rlutil::saveDefaultColor();
 
-	myutil::helloMother();
+    myutil::correctionConsole();
+    myutil::loadMap("../Pogaimon/assets/yzumap.txt");
 
-	// Test 9
-	int x = 7; int y = 7;
-	rlutil::cls();
-	rlutil::CursorHider curs;
-	std::cout << "Test 9: Arrow keys" << std::endl;
-	std::cout << "You should be able to move the '@' character with arrow keys." << std::endl;
-	std::cout << "Hit Escape to continue to the next test." << std::endl;
-	gotoxy(x, y); std::cout << '@' << std::endl; // Output player
-	while (true) {
-		if (kbhit()) {
-			int k = rlutil::getkey(); // Get character
-			gotoxy(x, y); std::cout << " "; // Erase player
-			if (k == rlutil::KEY_LEFT) --x;
-			else if (k == rlutil::KEY_RIGHT) ++x;
-			else if (k == rlutil::KEY_UP) --y;
-			else if (k == rlutil::KEY_DOWN) ++y;
-			else if (k == rlutil::KEY_ESCAPE) break;
-			gotoxy(x, y); std::cout << '@'; // Output player
-			std::cout.flush();
-		}
-	}
+	
 	
 	
 	return 0;
--- Pogaimon/Myutil.h
+++ Pogaimon/Myutil.h
@@ -1,12 +1,57 @@
 #pragma once
 #include <iostream>
+#include <string>
+#include <fstream>
+#include "rlutil.h"
 
 namespace myutil {
-	void inline helloMother(); // inline can resolve error LNK2005
+	//測試  // inline can resolve error LNK2005
+	void inline helloMother(); 
+	// 讀取 Map，並show在終端
+	void inline loadMap(std::string filename);
+    // 請使用者校正 console。
+    void inline correctionConsole();
 }
 
 
 void myutil::helloMother(){
 		std::cout << "hello" << std::endl;
-	
 }
+
+void myutil::loadMap(std::string filename)
+{
+    std::string line;
+    std::ifstream mapfile(filename);
+    if (mapfile.is_open())
+    {
+        while (getline(mapfile, line))
+        {
+            std::cout << line << '\n';
+        }
+        mapfile.close();
+    }
+    else { 
+        std::cout << "Unable to open file: " << "\"" << filename << "\"" << std::endl;
+    }
+
+}
+
+void myutil::correctionConsole()
+{
+    using std::cout;
+    using std::endl;
+    cout << "*************************************************"\
+         << "**************************************************" << endl;
+    for (int i = 0; i < 32; i++) {
+        cout << "*";
+        for (int i = 0; i < 96; i++) {
+            cout << " ";
+        } cout << "*" << endl;
+
+    }
+
+    cout << "Please make this line to \"ONE\" row." << endl;
+    rlutil::anykey("\n\nPress any key to continue...\n");
+    rlutil::cls();
+}
+
