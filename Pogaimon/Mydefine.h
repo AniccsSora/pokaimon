ac3a9a5b8cb0df25a6ff0e276d1ce0174866e720
aniccasora <aniccasora@gmail.com>
2020-05-27 15:08:36 +0800

    用 template 定義 vector <T>

---
+++ Pogaimon/Mydefine.h
@@ -0,0 +1,8 @@
+#pragma once
+
+template <typename T>
+using Vec_2D_ = std::vector<std::vector<T>>;
+//typedef std::vector<std::vector<std::string>> Vec_2D;
+
+template <typename T>
+using Vec_1D_ = std::vector<T>;
