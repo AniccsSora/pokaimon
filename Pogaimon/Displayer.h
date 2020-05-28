d4505cee0dfb0112691e1aecf7170cd6c51646af
AniccaSora <aniccasora@gmail.com>
2020-05-29 01:18:16 +0800

    Displayer 暫時定義完畢, 接下來嘗試 定義 ViewCreater。

--- Pogaimon/Displayer.h
+++ Pogaimon/Displayer.h
@@ -6,20 +6,21 @@ class Displayer
 {
 public:
 
-	// 給定 顯示座標。
+	// 啥事都不會做~~~
 	Displayer();
 
-	// 顯示 某個 View
-	void showView();
+	// 顯示 某個 View, 在 console 上。
+	void showView(std::string viewName);
 
-	// 增加某個 View 到 viewList 內。
+	// 增加某個 View 到 viewList 內。 (view 只加不減~)。
 	void addView(MySpace::View view);
 
 private:
-	//
-	
 
-	// 記錄著此 displayer 所控管的 View。
+	// 已經被使用的 畫面空間，從displayer 建立到現在的時間。
+	std::vector<MySpace::ViewStatus> usedSpace;
+
+	// 記錄著此 displayer 下所控管的 View。
 	std::vector<MySpace::View> viewList;
 };
 
