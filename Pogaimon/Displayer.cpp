4866dbdfa389515069d77b61ca79b2b36a596e2f
AniccaSora <aniccasora@gmail.com>
2020-06-06 18:55:24 +0800

    因應 print_c 做 showView 的改動。

--- Pogaimon/Displayer.cpp
+++ Pogaimon/Displayer.cpp
@@ -7,7 +7,9 @@ Displayer::Displayer(){
 
 // one-base
 void Displayer::showView(MySpace::ViewPtr view, short x, short y){
-	
+	//格式為: "{row}:{rlutil number}
+	std::vector<std::string> flg = view->color_flg;
+
 	rlutil::locate(x, y);
 	rlutil::saveDefaultColor();
 	for (size_t i = 0; i < (size_t)view->element.size(); ++i) {
@@ -26,6 +28,30 @@ void Displayer::showView(MySpace::ViewPtr view, short x, short y){
 					if (i == 0 || i == view->element.size() - 1 ||
 						j == 0 || j == view->element.at(i).size() - 1) {
 						rlutil::setColor(view->frameColor);
+					}
+					else if ( flg.size() ) { // View::color_flg 取得 是否要 印 特殊顏色
+						for (auto f : flg) {// 走訪 "{row}:{rlutil number};" 字串旗標
+							
+							int row_int = -1;//第幾行
+							int rlutil_cnumber = -1;//色彩代號
+
+							int split_1 = f.find(':'); // 尋找{row}: 的 冒號位置
+							int split_2 = f.find(';'); // 尋找{rlutil number}; 的 分號位置
+
+							// {row 字串 轉 int
+							row_int = std::atoi(f.substr(0, split_1).c_str());
+							// {rlutil number}字串 轉 int
+							rlutil_cnumber = std::atoi(f.substr(split_1+1, split_2-split_1-1).c_str());
+
+
+							if ( i == row_int){ // 比對 ( 列 && 旗標的 {row}
+								rlutil::setColor(rlutil_cnumber);
+								break; //不然他會跳到別的  "{row}:{rlutil number};" 反而走去 else 又設回原本顏色。
+							}
+							else {
+								rlutil::setColor(view->msgColor);
+							}
+						}
 					}
 					else {
 						rlutil::setColor(view->msgColor);
