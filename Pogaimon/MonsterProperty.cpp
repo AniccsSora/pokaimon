8033c967a885a3a71407a094fda454caab1481cf
anicca <1080Ti>
2020-06-11 16:27:07 +0800

    增加新函式

--- Pogaimon/MonsterProperty.cpp
+++ Pogaimon/MonsterProperty.cpp
@@ -1,5 +1,21 @@
 #include "MonsterProperty.h"
 
+MonsterProperty* MonsterProperty::operator=(MonsterProperty& pp)
+{
+	this->idx = pp.idx;
+	this->name = pp.name;
+	this->type = pp.type;
+	this->typeStr = pp.typeStr;
+	this->hp = pp.hp;
+	this->atk = pp.atk;
+	this->def = pp.def;
+	this->speed = pp.speed;
+	this->ability = pp.ability;
+	this->sk_Immun_propertyDebuff = pp.sk_Immun_propertyDebuff;
+	this->sk_usedSkill_cnt = pp.sk_usedSkill_cnt;
+	return this;
+}
+
 MonsterProperty::MonsterProperty(const MonsterProperty& mp){
 	this->idx = mp.idx;
 	this->name = mp.name;
@@ -10,4 +26,56 @@ MonsterProperty::MonsterProperty(const MonsterProperty& mp){
 	this->def = mp.def;
 	this->speed = mp.speed;
 	this->ability = mp.ability;
+}
+
+
+void MonsterProperty::reduceAbility(propertyType type, int reduce_value)
+{
+	if ( canReduceAbility() ) {
+		if (type == HP) {
+			this->hp -= reduce_value;
+		}
+		else if (type == ATK){
+			this->atk -= reduce_value;
+		}
+		else if (type == DEF) {
+			this->def -= reduce_value;
+		}
+		else if (type == SPEED) {
+			this->speed -= reduce_value;
+		}
+		else {
+			throw UNKNOW_propertyType(type);
+		}
+		return;
+	}
+	else {// 即使擁有 免疫降低能力值 之技能，HP 仍要可以被扣。
+		if (type == HP) {
+			this->hp -= reduce_value;
+		}
+		else {
+			// 欲降低 HP以外的 指令都不會被執行...
+			return;
+		}
+	}
+}
+
+void MonsterProperty::increaseAbility(propertyType type, int increase_value)
+{
+	if (type == HP) {
+		this->hp += increase_value;
+	}
+	else if (type == ATK) {
+		this->atk += increase_value;
+	}
+	else if (type == DEF) {
+		this->def += increase_value;
+	}
+	else if (type == SPEED) {
+		this->speed += increase_value;
+	}
+	else {
+		throw UNKNOW_propertyType(type);
+	}
+	return;
 }
--- Pogaimon/MonsterProperty.h
+++ Pogaimon/MonsterProperty.h
@@ -25,9 +25,60 @@ enum monType{
 	Fairy
 };
 
+// 能力數值代號
+enum propertyType {
+	UNK = -1,
+	HP,
+	ATK,
+	DEF,
+	SPEED,
+};
+
+// 例外類別
+class UNKNOW_propertyType : public std::exception {
+public:
+	propertyType type = propertyType::UNK;
+
+	// 建構子
+	UNKNOW_propertyType(propertyType type) {
+		this->type = type;
+	}
+
+	virtual const char* what() const throw()
+	{
+		return std::string("UNKNOW_propertyType" + std::to_string(type)).c_str();
+	}
+};
 
 class Property {
+public:
 
+	// 統一用此方法來降低 "能力數值"。
+	// @ type: 要降低的屬性。
+	// @ reduce_value: 要降低多少數值。
+	virtual void reduceAbility(propertyType type, int reduce_value) = 0;
+	
+	// 統一用此方法來增加 "能力數值"。
+	// @ type: 要增加 的屬性。
+	// @ reduce_value: 要增加多少數值。
+	virtual void increaseAbility(propertyType type, int increase_value) = 0;
+protected:
+	// 這邊的參數 是給 怪物與怪物 之間 技能互動所用到的私有變數，如果沒有用到 都會是 -1。
+	// 並且由多個 私有函式 幫助確認。(檢查 ability)
+
+	// 有一些技能會 只能發動一次，或者固定次數， -1 代表不屬於此技能，0代表屬於且尚未使用， 大於0代表已經觸發過 此數 次
+	int sk_usedSkill_cnt = -1;
+
+	// 特殊狀態 抗性，免疫降低能力值的特殊能力(HP,ATK,DEF,SPEED, )
+	int sk_Immun_propertyDebuff = -1;
+
+	// 使否可以降低能力。
+	bool canReduceAbility() {
+		if (sk_Immun_propertyDebuff <= 0) {
+			return true;
+		}
+		else { return false; }
+	}
 };
 
 class MonsterProperty :	public Property{
@@ -36,6 +87,7 @@ public:
 	// Jym 需要直接做 參數操作。
 	friend class Jym;
 
+	MonsterProperty* operator=(MonsterProperty& pp);
 	MonsterProperty() = default; // compile 說我沒寫預設建構子，好 我寫給他。
 	MonsterProperty(int idx, std::string name, std::string type, int hp, int atk, int def, int speed, int ability) 
 		:Property (){
@@ -109,6 +161,7 @@ public:
 		this->speed = speed;
 	}
 
+	// this idx is 1-base;
 	int getAbilityIdx() {
 		return ability;
 	}
@@ -195,33 +248,30 @@ public:
 			rtnType = Fairy;
 		}
 		else {
-			std::cout << "UNHANDLE ENUM : " << "\"" << typeUpper << "\"" << std::endl;
+			std::cout << "UNHANDLE ENUM \'monType\': " << "\"" << typeUpper << "\"" << std::endl;
 			std::system("pause");
 			std::exit(8740);
 		} 
 		
 		return rtnType;
 	}
+
+	// 降低指定數值
+	void reduceAbility(propertyType type, int reduce_value)override;
+
+	void  increaseAbility(propertyType type, int increase_value)override;
+
 private:
 	int idx = -1;
 	std::string name = "";
-	monType type = Undefined;
+	monType type = monType::Undefined;
 	std::string typeStr = "";
+
 	int hp = -1;
 	int atk = -1;
 	int def = -1;
 	int speed = -1;
 	int ability = -1;
-
-	// 這邊的參數 是給 怪物與怪物 之間 技能互動所用到的私有變數，如果沒有用到 都會是 -1。
-	// 並且由多個 私有函式 幫助確認。(檢查 ability)
-
-	// 有一些技能會 只能發動一次，或者固定次數， -1 代表不屬於此技能，0代表屬於且尚未使用， 大於0代表已經觸發過 此數 次
-	int sk_usedSkill_cnt = -1; 
-	// 特殊狀態 抗性，免疫降低能力值得特殊能力
-	int sk_Immun_propertyDebuff = -1;
-
-	// 提供方法來 扣除或者 增加能力??
 
 };
 
